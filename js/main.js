3ed17484c05d9598382da0842c148fa39d135882
Ryonosuke Miyashits <ryonosuke-miyashita@prime-skateboard.com>
2025-11-13 17:58:14 +0900

    お問合せボタン実装

--- js/main.js
+++ js/main.js
@@ -4,3 +4,31 @@ const nav = document.querySelector(".global-nav");
 toggleButton.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+
+document.getElementById('trigger-contact')?.addEventListener('click', e => {
+  e.preventDefault();
+  try {
+    const target = document.getElementById(e.currentTarget.value);
+    
+    if (!target) throw new Error('Does Not Exists TargetElement.');
+
+    target.classList.toggle('disabled');
+    document.body.classList.toggle('disabled');
+  } catch (_e) {
+    console.error(_e);
+  }
+});
+document.getElementById('remover-contact')?.addEventListener('click', e => {
+  e.preventDefault();
+  try {
+    const target = document.getElementById(e.currentTarget.value);
+    
+    if (!target) throw new Error('Does Not Exists TargetElement.');
+
+    target.classList.toggle('disabled');
+    document.body.classList.toggle('disabled');
+  } catch (_e) {
+    console.error(_e);
+  }
+});
+
