3228f5659cf4a289b347135ce106b6e9963e1a27
Ryonosuke Miyashits <ryonosuke-miyashita@prime-skateboard.com>
2025-11-16 17:48:49 +0900

    ポップアップ処理を削除

--- js/main.js
+++ js/main.js
@@ -17,33 +17,6 @@ toggleButton.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
 
-document.getElementById('trigger-contact')?.addEventListener('click', e => {
-  e.preventDefault();
-  try {
-    const target = document.getElementById(e.currentTarget.value);
-    
-    if (!target) throw new Error('Does Not Exists TargetElement.');
-
-    target.classList.toggle('disabled');
-    document.body.classList.toggle('disabled');
-  } catch (_e) {
-    console.error(_e);
-  }
-});
-document.getElementById('remover-contact')?.addEventListener('click', e => {
-  e.preventDefault();
-  try {
-    const target = document.getElementById(e.currentTarget.value);
-    
-    if (!target) throw new Error('Does Not Exists TargetElement.');
-
-    target.classList.toggle('disabled');
-    document.body.classList.toggle('disabled');
-  } catch (_e) {
-    console.error(_e);
-  }
-});
-
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
